2a2a0830095a33a3a2cbc5917018ef2108c3af01
harahehe <mrxhasvih@gmail.com>
2025-12-22 14:46:55 -0500

    Update pins.h

--- ESP32/pins.h
+++ ESP32/pins.h
@@ -1,24 +1,27 @@
 // Pin assignments for ESP32 greenhouse v2
-// DHT22 (ambient temperature / humidity)
-#define PIN_DHT       4
+
 // OneWire bus for two DS18B20 sensors
-// #define PIN_ONEWIRE   17
+// #define PIN_ONEWIRE   16   // 2x DS18B20 en el mismo bus
 
-// Relays
-#define PIN_RELE1     26  // Relay 1 (pump)
-#define PIN_RELE2     27  // Relay 2 (light)
+// --- Sensores lejos de la planta (fila superior) ---
+#define PIN_DHT       4
 
-// Analog sensors
-#define PIN_MQ135     34  // MQ-135 (analog input)
-#define PIN_SUELO     35  // Soil moisture (analog input)
-#define PIN_FLOAT     33  // Float switch (water level, digital)
+// ADC1 (seguros con WiFi)
+#define PIN_MQ135     34   // input-only
+#define PIN_SUELO     32   // ADC1
 
-// PWM for fans
-#define PIN_FAN_PWM   25  // PWM to MOSFET (fans)
-
-// SD card (SPI)
-#define PIN_SD_CS     5   // SD chip select
-
-// I2C for RTC
+// I2C RTC
 #define PIN_I2C_SDA   21
 #define PIN_I2C_SCL   22
+
+// --- SD card (SPI remapeado) ---
+#define PIN_SD_CS     5
+// #define PIN_SD_SCK    14
+// #define PIN_SD_MISO   19
+// #define PIN_SD_MOSI   23
+
+// --- Cerca de la planta (fila inferior) ---
+#define PIN_FAN_PWM   25   // PWM MOSFET (fans)
+#define PIN_RELE1     17   // MOSFET bomba DC
+#define PIN_RELE2     13   // luz AC (relé)
+#define PIN_FLOAT     18   // Float switch
