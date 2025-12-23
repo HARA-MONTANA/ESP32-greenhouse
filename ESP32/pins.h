c88e1909fe61c847a12eeea8091b557d62baeca4
harahehe <mrxhasvih@gmail.com>
2025-12-23 08:38:08 -0500

    Update pins.h

--- ESP32/pins.h
+++ ESP32/pins.h
@@ -1,10 +1,10 @@
 // Pin assignments for ESP32 greenhouse v2
 
 // OneWire bus for two DS18B20 sensors
-// #define PIN_ONEWIRE   16   // 2x DS18B20 en el mismo bus
+// #define PIN_ONEWIRE   18   // 2x DS18B20 en el mismo bus
 
 // --- Sensores lejos de la planta (fila superior) ---
-#define PIN_DHT       4
+#define PIN_DHT       25
 
 // ADC1 (seguros con WiFi)
 #define PIN_MQ135     34   // input-only
@@ -21,7 +21,7 @@
 // #define PIN_SD_MOSI   23
 
 // --- Cerca de la planta (fila inferior) ---
-#define PIN_FAN_PWM   25   // PWM MOSFET (fans)
+#define PIN_FAN_PWM   16   // PWM MOSFET (fans)
 #define PIN_RELE1     17   // MOSFET bomba DC
 #define PIN_RELE2     13   // luz AC (relé)
-#define PIN_FLOAT     18   // Float switch
+#define PIN_FLOAT     4   // Float switch
