e7557535be0c7a9625ed7d1aa8ebf392e1dcc658
harahehe <mrxhasvih@gmail.com>
2025-12-23 13:15:24 -0500

    Actualizar pins.h

--- ESP32/pins.h
+++ ESP32/pins.h
@@ -22,7 +22,7 @@
 // #define PIN_SD_MOSI   23
 
 // --- Cerca de la planta (fila inferior) ---
-#define PIN_FAN_PWM   16   // PWM MOSFET (fans)
+#define PIN_FAN_PWM   16   // MOSFET (fans)
 #define PIN_RELE1     17   // MOSFET bomba DC
 #define PIN_RELE2     13   // luz AC (relé)
 #define PIN_FLOAT     4   // Float switch
